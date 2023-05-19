0370094a2ef45d4a139b5c40c885dcbe3f177980
nam <you@example.com>
2023-05-19 14:54:39 +0700

    Add section testimonials and team

--- js/main.js
+++ js/main.js
@@ -9,6 +9,28 @@
     //     dalay: 10,
     //     time: 3000,
     // });
+
+    // Owl-Carousel js
+    $('.testimonials__carousel').owlCarousel({
+        autoplay: true,
+        loop: true,
+        smartSpeed: 1500,
+        dots: true,
+        nav: false,
+        center: true,
+        margin: 24,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
    
 })(jQuery);
 
